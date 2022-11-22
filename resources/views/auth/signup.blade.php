196d8ea6aace41b4fe6c8d5c38c5ad7312fc7eae
Xavier Stouder <xavier@stouder.io>
2022-11-22 14:43:35 +0100

    chore: migrate register layout to bootstrap

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -7,22 +7,22 @@
 @section('content')
     <form action="/signup" method="post">
         @csrf
-        <div class="form-group">
-            <label for="email" class="form-item">Adresse email</label>
-            <input id="email" class="form-item" name="email" type="email" required>
+        <div class="mb-3">
+            <label for="email" class="form-label">Adresse email</label>
+            <input id="email" class="form-control" name="email" type="email" required>
         </div>
-        <div class="form-group">
-            <label for="password" class="form-item">Mot de passe</label>
-            <input id="password" class="form-item" name="password" type="password" required>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input id="password" class="form-control" name="password" type="password" required>
         </div>
-        <div class="form-group">
-            <label for="name" class="form-item">Nom d'utilisateur</label>
-            <input id="name" class="form-item" name="name" type="text" required>
+        <div class="mb-3">
+            <label for="name" class="form-label">Nom d'utilisateur</label>
+            <input id="name" class="form-control" name="name" type="text" required>
         </div>
-        <div class="form-group">
-            <label for="description" class="form-item">Description</label>
-            <textarea id="description" class="form-item" name="description"></textarea>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea id="description" class="form-control" name="description"></textarea>
         </div>
-        <button type="submit" class="btn form-item">Créer mon compte</button>
+        <button type="submit" class="btn btn-primary">Créer mon compte</button>
     </form>
 @endsection
