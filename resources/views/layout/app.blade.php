b95eede727ffb436fba5bb6cb167a13ffe41c7c5
Xavier Stouder <xavier@stouder.io>
2022-11-22 14:57:57 +0100

    fix: it was the wrong container

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -33,14 +33,14 @@
         </ul>
     </header>
     <main class="flex-shrink-0">
-        <h1>@yield('title')</h1>
-        @if (session('success'))
-            <p class="msgbox msg-success">{{ session('success') }}</p>
-        @endif
-        @if (session('error'))
-            <p class="msgbox msg-error">{{ session('error') }}</p>
-        @endif
         <div class="container">
+            <h1>@yield('title')</h1>
+            @if (session('success'))
+                <p class="msgbox msg-success">{{ session('success') }}</p>
+            @endif
+            @if (session('error'))
+                <p class="msgbox msg-error">{{ session('error') }}</p>
+            @endif
             @yield('content')
         </div>
     </main>
