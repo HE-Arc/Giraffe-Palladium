f4dbb9ee4f184117f8ee702187fbe6df3920c5a7
Xavier Stouder <xavier@stouder.io>
2022-11-22 14:42:21 +0100

    chore: migrate login layout

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -7,13 +7,13 @@
 @section('content')
     <form action="/signin" method="post">
         @csrf
-        <div class="form-group">
-            <label for="email" class="form-item">Adresse email</label>
-            <input id="email" class="form-item" name="email" type="email" required>
+        <div class="mb-3">
+            <label for="email" class="form-label">Adresse email</label>
+            <input id="email" class="form-control" name="email" type="email" required>
         </div>
-        <div class="form-group">
-            <label for="password" class="form-item">Mot de passe</label>
-            <input id="password" class="form-item" name="password" type="password" required>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input id="password" class="form-control" name="password" type="password" required>
         </div>
-        <button type="submit" class="btn form-item">Se connecter</button>
+        <button type="submit" class="btn btn-primary">Se connecter</button>
     @endsection
