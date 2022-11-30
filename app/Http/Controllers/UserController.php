9fbba6298e8c4216fea4dcd314623832486cb6ef
Ozurah <80265286+Ozurah@users.noreply.github.com>
2022-11-30 16:34:52 +0100

    feat: add component

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,6 +70,7 @@ class UserController extends Controller
 
     public function borrows(User $user)
     {
+        $this->authorize('borrows', $user);
         return view('users.borrows', [
             'user' => $user,
             'borrows' => $user->borrows()->orderby("deadline")->get(),
