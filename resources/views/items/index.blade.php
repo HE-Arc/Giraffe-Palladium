168dfbba47ac218bb958338a350d3b3d8e17471a
sebastie.chappuis3 <sebastien.chappuis1@he-arc.ch>
2022-11-30 16:31:44 +0100

    feat: added list of borrowable items

---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('title')
+    Liste des objets empruntables
+@endsection
+
+@section('content')
+    <ul>
+        @foreach ($items as $item)
+            <li>
+                <!-- TODO: use the item component -->
+                <a href="{{ route('items.show', $item->id) }}">{{ $item->title }}</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::delete('/asks/{ask}', [AskController::class, 'destroy'])->name('asks.dest
 // ItemsController
 Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+Route::get('/items/borrowable', [ItemController::class, 'index'])->name('items.index');
 Route::get('/items/{item}', [ItemController::class, 'show'])->name('items.show');
 Route::get('/items/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
 Route::put('/items/{item}', [ItemController::class, 'update'])->name('items.update');
