3b9552bfe2b30975fd36ffda595b55635d27366a
Jonas Allemann <jonas.allemann@he-arc.ch>
2022-12-03 22:53:36 +0100

    feat: the home page resume firsts shares

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $nbShow = 3;
+        $user = User::find(Auth::id());
+        // $user = Auth::user(); // This is the same as the line above, but VSC doesn't did the type casting to User, so no intellisense
+
+        if ($user){
+            $lends = $user->lends()->orderby("deadline")->take($nbShow)->get();
+            $borrows = $user->borrows()->orderby("deadline")->take($nbShow)->get();
+        }
+        return view('home',  [
+            'user' => $user,
+            'lends' => $lends ?? null,
+            'borrows' => $borrows ?? null,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,58 @@
 
 @section('content')
     <p>Bienvenue sur la page d'accueil !</p>
+
+    @if (!$user)
+        <p>
+            Vous n'êtes pas connecté.<br>
+            Pour gérez vos partages, veuillez vous connecté
+        </p>
+    @else
+        <p>Ravi de vous revoir {{ $user->name }} !</p>
+        <p>En ce {{ now()->format('d') }} {{ \Carbon\Carbon::now()->locale('fr_FR')->monthName }}, vous avez :</p>
+
+        <ul>
+            <li>
+                <a href=""> {{-- TODO : redirect to the page when it is created (issue #5) --}}
+                    {{ $user->asks->count() }} demande(s) d'emprunt
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('users.lends', Auth::user()->id) }}"">
+                    {{ $user->lends->count() }} prêt(s) en cours
+                </a>
+                <div>
+                    <ul>
+                        @if ($lends->count() > 0)
+                            @foreach ($lends as $lend) {{-- type of : Share --}}
+                                <x-share
+                                    :title="$lend->item->title"
+                                    :name="$lend->borrower ? $lend->borrower->name : $lend->nonuser_borrower"
+                                    :deadline="$lend->deadline"
+                                />
+                            @endforeach
+                        @endif
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="{{ route('users.borrows', Auth::user()->id) }}">
+                    {{ $user->borrows->count() }} emprunt(s) en cours
+                </a>
+                <div>
+                    <ul>
+                        @if ($borrows->count() > 0)
+                            @foreach ($borrows as $borrow) {{-- type of : Share --}}
+                                <x-share
+                                    :title="$borrow->item->title"
+                                    :name="$borrow->lender ? $borrow->lender->name : $borrow->nonuser_lender"
+                                    :deadline="$borrow->deadline"
+                                />
+                            @endforeach
+                        @endif
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    @endif
 @endsection
