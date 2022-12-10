989c6bb61bceb40d9b681bcaac7fa09a7af0b2be
Jonas Allemann <jonas.allemann@he-arc.ch>
2022-12-10 23:49:43 +0100

    feat: add some text colorization

--- public/js/shares/edit.js
+++ public/js/shares/edit.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const userInput = document.getElementById('otherUser');
+const deadlineInput = document.getElementById('deadline');
+
+function setColorUserInput() {
+    const classColor = 'text-primary'; // Bootstrap class for text color
+    if (userInput.value[0] === '@') {
+        userInput.classList.add(classColor);
+    }else{
+        userInput.classList.remove(classColor);
+    }
+}
+
+function setColorDeadlineInput() {
+    const classColor = 'text-danger'; // Bootstrap class for text color
+    // if deadline passed
+    if (deadlineInput.value != "" && deadlineInput.value < new Date().toISOString().split('T')[0]) {
+        deadlineInput.classList.add(classColor);
+    }else{
+        deadlineInput.classList.remove(classColor);
+    }
+}
+
+deadlineInput.onchange = function(){setColorDeadlineInput()};
+setColorDeadlineInput();
+
+userInput.onkeyup = function(){setColorUserInput()};
+setColorUserInput();
+
