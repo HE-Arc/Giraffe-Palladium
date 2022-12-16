694212a1b74161d68bef3a37d56cd395058cee25
sebastie.chappuis3 <sebastien.chappuis1@he-arc.ch>
2022-12-16 12:21:55 +0100

    fix: indentation + bold username

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,13 +5,13 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <ul class="row p-0">
+    <div class="container">
+        <ul class="row p-0">
             @foreach ($users as $user)
                 <li class="col-lg-6 list-unstyled p-0">
                     <a href="{{ route('users.show', $user->id) }}" class="d-block link-dark
                         rounded-2 border border-gray bg-light text-decoration-none m-1 p-3">
-                        <p class="text-center m-0">{{ $user->name }}</p>
+                        <p class="text-center m-0"><strong>{{ $user->name }}</strong></p>
                         <p class="text-center m-0 my-1">{{ $user->email }}</p>
                         <p class="text-center m-0">{{ $user->getNumberBorrowableItems() }} objet(s) proposé(s)</p>
                     </a>
